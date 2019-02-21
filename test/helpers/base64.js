0148a671528ab4c4c263f3fc4810af069e9398a3
Tak Tran <contact@tutaktran.com>
2019-02-21 11:08:38 +0000

    Refactor test to use base64Encode

--- test/helpers/base64.js
+++ test/helpers/base64.js
@@ -1,6 +1,8 @@
-const base64EncodeObj = obj =>
-	Buffer.from(JSON.stringify(obj)).toString('base64');
+const base64Encode = str => Buffer.from(str).toString('base64');
+
+const base64EncodeObj = obj => base64Encode(JSON.stringify(obj));
 
 module.exports = {
+	base64Encode,
 	base64EncodeObj
 };

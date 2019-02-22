c55186305372d732f32f2d10c1baebe187c3eb7d
Tak Tran <contact@tutaktran.com>
2019-02-22 10:37:41 +0000

    Ignore empty lines from input

--- test/lib/get-repositories.test.js
+++ test/lib/get-repositories.test.js
@@ -2,6 +2,13 @@ const createStandardInput = require('../helpers/create-standard-input');
 const getRepositories = require('../../lib/get-repositories');
 
 describe('getRepositories', () => {
+	test('no input', () => {
+		createStandardInput();
+
+		const repos = getRepositories();
+		expect(repos).toEqual([]);
+	});
+
 	test('works for single repository', () => {
 		createStandardInput('something');
 
@@ -15,5 +22,12 @@ describe('getRepositories', () => {
 		const [firstRepo, secondRepo] = getRepositories();
 		expect(firstRepo).toEqual('something');
 		expect(secondRepo).toEqual('something-else');
+	});
+
+	test('ignores empty lines', () => {
+		createStandardInput('something\n\n');
+
+		const repos = getRepositories();
+		expect(repos).toHaveLength(1);
 	});
 });

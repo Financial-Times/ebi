6f126fcaded38aced870cf72e988808eca16e2db
Jennifer Johnson <jennifer.kerr@ft.com>
2019-02-25 14:31:43 +0000

    fix limit bug (#56)
    * fix limit bug
    
    * update limit yargs config
    
    * Update src/commands/shared.js
    
    Co-Authored-By: jkerr321 <jennifer.kerr@ft.com>
    
    * Update src/commands/shared.js
    
    Co-Authored-By: jkerr321 <jennifer.kerr@ft.com>

--- src/commands/shared.js
+++ src/commands/shared.js
@@ -1,9 +1,7 @@
 const withLimit = yargs => {
-	return yargs.option('token', {
-		required: true,
-		type: 'string',
-		describe:
-			'GitHub personal access token. Generate one from https://github.com/settings/tokens'
+	return yargs.option('limit', {
+		type: 'number',
+		describe: 'Limit the number of repositories to search for'
 	});
 };
 

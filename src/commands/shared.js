f0d268c06a4fa979dc8e4c5bcfbf99a56160b37a
Jennifer Kerr <jkerr321@gmail.com>
2019-02-25 14:17:17 +0000

    update limit yargs config

--- src/commands/shared.js
+++ src/commands/shared.js
@@ -1,9 +1,8 @@
 const withLimit = yargs => {
 	return yargs.option('limit', {
 		required: true,
-		type: 'string',
-		describe:
-			'GitHub personal access token. Generate one from https://github.com/settings/tokens'
+		type: 'number',
+		describe: 'limit the number of repos to return to this number'
 	});
 };
 

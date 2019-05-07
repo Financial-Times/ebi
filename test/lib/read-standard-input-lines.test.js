37bdb0d0b56aee108f06fc8d3e9e10a62abd66c3
Tak Tran <contact@tutaktran.com>
2019-05-07 17:41:02 +0100

    Add test for readStardardInputLines
    Also ignore empty lines. Not going to need it downstream.

---
+++ test/lib/read-standard-input-lines.test.js
@@ -0,0 +1,39 @@
+const setupReadline = require('../helpers/setup-readline');
+const readStardardInputLines = require('../../lib/read-standard-input-lines');
+
+const setupReadStdin = async input => {
+	const { readString, teardown } = setupReadline();
+	const read = readStardardInputLines();
+	readString(input);
+	const lines = await read;
+	teardown();
+
+	return lines;
+};
+
+describe('readStardardInputLines', () => {
+	test('outputs empty array with no input', async () => {
+		const lines = await setupReadStdin();
+		expect(lines).toEqual([]);
+	});
+
+	test('outputs empty array with empty string', async () => {
+		const lines = await setupReadStdin('');
+		expect(lines).toEqual([]);
+	});
+
+	test('outputs one line', async () => {
+		const lines = await setupReadStdin('something');
+		expect(lines).toEqual(['something']);
+	});
+
+	test('outputs multiple lines', async () => {
+		const lines = await setupReadStdin('something\nsomething2');
+		expect(lines).toEqual(['something', 'something2']);
+	});
+
+	test('outputs ignores empty lines', async () => {
+		const lines = await setupReadStdin('something\n\nsomething2');
+		expect(lines).toEqual(['something', 'something2']);
+	});
+});

1ab62067319ab6bbc658c07b11deaee52b13d83b
Tak Tran <contact@tutaktran.com>
2019-05-08 17:00:48 +0100

    Support no stdin in readStardardInputLines
    Allow custom `createInterface` implementation to be passed into
    `setupReadline` to be able to simulate no stdin.

--- test/helpers/setup-readline.js
+++ test/helpers/setup-readline.js
@@ -4,24 +4,29 @@ jest.mock('readline');
 
 /**
  * Mock implementation of readline
+ *
+ * @param {function} createInterface - Mock implementation of `readline.createInterface`. Uses a default implementation if not specified
  */
-const setupReadline = () => {
+const setupReadline = ({ createInterface } = {}) => {
 	let lineFn = () => {};
 	let closeFn = () => {};
-	const readlineInterface = {
-		// Keep a reference of the line and close function, so it can
-		// be run later
-		on: (event, fn) => {
-			if (event === 'line') {
-				lineFn = fn;
-			} else if (event === 'close') {
-				closeFn = fn;
-			}
-			return readlineInterface;
-		}
-	};
 
-	readline.createInterface.mockImplementation(() => readlineInterface);
+	const mockReadlineInterface = !!createInterface
+		? createInterface()
+		: {
+				// Keep a reference of the line and close function, so it can
+				// be run later
+				on: (event, fn) => {
+					if (event === 'line') {
+						lineFn = fn;
+					} else if (event === 'close') {
+						closeFn = fn;
+					}
+					return mockReadlineInterface;
+				}
+		  };
+
+	readline.createInterface.mockImplementation(() => mockReadlineInterface);
 	const initialIsTTY = process.stdin.isTTY;
 	// Allow standard input to be detected
 	process.stdin.isTTY = false;

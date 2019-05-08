bd6ac658b0f629a3a089109be2370fae9d2564b3
Tak Tran <contact@tutaktran.com>
2019-05-08 16:41:02 +0100

    Refactor setupReadStdin to use obj args

--- test/lib/read-standard-input-lines.test.js
+++ test/lib/read-standard-input-lines.test.js
@@ -1,7 +1,7 @@
 const setupReadline = require('../helpers/setup-readline');
 const readStardardInputLines = require('../../lib/read-standard-input-lines');
 
-const setupReadStdin = async input => {
+const setupReadStdin = async ({ input } = {}) => {
 	const { readString, teardown } = setupReadline();
 	const read = readStardardInputLines();
 	readString(input);
@@ -18,22 +18,24 @@ describe('readStardardInputLines', () => {
 	});
 
 	test('outputs empty array with empty string', async () => {
-		const lines = await setupReadStdin('');
+		const lines = await setupReadStdin({ input: '' });
 		expect(lines).toEqual([]);
 	});
 
 	test('outputs one line', async () => {
-		const lines = await setupReadStdin('something');
+		const lines = await setupReadStdin({ input: 'something' });
 		expect(lines).toEqual(['something']);
 	});
 
 	test('outputs multiple lines', async () => {
-		const lines = await setupReadStdin('something\nsomething2');
+		const lines = await setupReadStdin({ input: 'something\nsomething2' });
 		expect(lines).toEqual(['something', 'something2']);
 	});
 
 	test('outputs ignores empty lines', async () => {
-		const lines = await setupReadStdin('something\n\nsomething2');
+		const lines = await setupReadStdin({
+			input: 'something\n\nsomething2'
+		});
 		expect(lines).toEqual(['something', 'something2']);
 	});
 });
